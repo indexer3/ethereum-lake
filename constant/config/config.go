63995e8d58c77c54c2b55e4da3267cc24d3cf8cb
J1ang <fenghaojiang97@m.scnu.edu.cn>
2023-06-23 01:05:51 +0800

    feat: add engine design

--- constant/config/config.go
+++ constant/config/config.go
@@ -5,10 +5,16 @@ const (
 
 	ClickHouseHost           = "clickhouse-host"
 	ClickHouseUser           = "clickhouse-user"
+	ClickHousePassword       = "clickhouse-password"
 	ClickHouseBatchWriteSize = "clickhouse-batch-write-size"
 
 	MySQLHost           = "mysql-host"
+	MySQLUser           = "mysql-user"
+	MySQLPassword       = "mysql-password"
 	MySQLBatchWriteSize = "mysql-batch-write-size"
 
+	PostgresHost           = "postgres-host"
+	PostgresUser           = "postgres-user"
+	PostgresPassword       = "postgres-password"
 	PostgresBatchWriteSize = "postgres-batch-write-size"
 )
--- engine/engine.go
+++ engine/engine.go
@@ -1,1 +1,19 @@
 package engine
+
+import "context"
+
+type IEngine interface {
+	Start(ctx context.Context) error
+	Stop(ctx context.Context)
+}
+
+type Engine struct {
+}
+
+func (e *Engine) Start(ctx context.Context) error {
+	return nil
+}
+
+func (e *Engine) Stop(ctx context.Context) {
+
+}
