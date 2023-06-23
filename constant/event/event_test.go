c82a0f72b55d972a8bd565df0c1deb6f75f41a26
J1ang <fenghaojiang97@m.scnu.edu.cn>
2023-06-23 12:14:28 +0800

    fix: fix missing test

--- constant/event/event_test.go
+++ constant/event/event_test.go
@@ -16,7 +16,7 @@ func TestOnEventHash(t *testing.T) {
 	})
 
 	t.Run("test on weth event hash", func(t *testing.T) {
-		assert.Equal(t, common.HexToHash(""), WithdrawalEvent)
-		assert.Equal(t, common.HexToHash(""), DepositEvent)
+		assert.Equal(t, common.HexToHash("0x7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b65"), WithdrawalEvent)
+		assert.Equal(t, common.HexToHash("0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c"), DepositEvent)
 	})
 }
