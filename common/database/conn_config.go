f487d6b5cda3910c265497a015d37df47398baa2
J1ang <fenghaojiang97@m.scnu.edu.cn>
2023-06-23 17:25:49 +0800

    fix: fix lint and test

--- common/database/conn_config.go
+++ common/database/conn_config.go
@@ -13,9 +13,9 @@ type ConnectionConfig struct {
 func (c ConnectionConfig) DSN(dbType DatabaseType) (string, error) {
 	switch dbType {
 	case ClickHouse:
-		return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", c.Username, c.Password, c.Host, c.Port, c.Database), nil
-	case Postgres:
 		return fmt.Sprintf("tcp://%s:%s@%s:%s?database=%s", c.Username, c.Password, c.Host, c.Port, c.Database), nil
+	case Postgres:
+		return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", c.Username, c.Password, c.Host, c.Port, c.Database), nil
 	case MySQL:
 		return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True", c.Username, c.Password, c.Host, c.Port, c.Database), nil
 	}
--- common/database/conn_config_test.go
+++ common/database/conn_config_test.go
@@ -9,15 +9,17 @@ import (
 func TestOnConnConfig(t *testing.T) {
 	t.Run("test on build postgres dsn", func(t *testing.T) {
 		connConfig := ConnectionConfig{
-			
+			Host:     "localhost",
+			Port:     "4000",
+			Username: "postgres",
+			Password: "password",
+			Database: "database",
 		}
 
 		dsn, err := connConfig.DSN(Postgres)
 		assert.NoError(t, err)
 
-		assert.Equal(t, dsn, "")
+		assert.Equal(t, dsn, "postgres://postgres:password@localhost:4000/database?sslmode=disable")
 	})
-
-
 
 }
