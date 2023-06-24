3455e40eb023747b8c90ef6fec8e3bb3736a0ff2
J1ang <fenghaojiang97@m.scnu.edu.cn>
2023-06-24 17:48:57 +0800

    feat: add uniswap contracts

--- contracts/contracts.go
+++ contracts/contracts.go
@@ -10,4 +10,12 @@ const (
 	ContractTypeSeaport       ContractType = "Seaport"
 	ContractTypeMulticall     ContractType = "Multicall"
 	ContractTypeOneInchOracle ContractType = "OneInchOracle"
+
+	ContractTypeUniswapV2Factory ContractType = "UniswapV2Factory"
+	ContractTypeUniswapV2Pair    ContractType = "UniswapV2Pair"
+
+	ContractTypeUniswapV3Factory            ContractType = "UniswapV3Factory"
+	ContractTypeUniswapV3Router             ContractType = "UniswapV3Router"
+	ContractTypeUniswapV3Pool               ContractType = "UniswapV3Pool"
+	ContractTypeUniswapV3NFTPositionManager ContractType = "UniswapV3NFTPositionManager"
 )
