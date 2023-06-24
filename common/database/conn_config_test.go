a5ec16f5ab18a74eec14a68baee5790a1839f131
J1ang <fenghaojiang97@m.scnu.edu.cn>
2023-06-24 14:40:49 +0800

    chore: add database connection dsn

--- common/database/conn_config_test.go
+++ common/database/conn_config_test.go
@@ -22,4 +22,33 @@ func TestOnConnConfig(t *testing.T) {
 		assert.Equal(t, dsn, "postgres://postgres:password@localhost:4000/database?sslmode=disable")
 	})
 
+	t.Run("test on build mysql dsn", func(t *testing.T) {
+		connConfig := ConnectionConfig{
+			Host:     "localhost",
+			Port:     "4000",
+			Username: "user",
+			Password: "password",
+			Database: "database",
+		}
+		dsn, err := connConfig.DSN(MySQL)
+		assert.NoError(t, err)
+
+		assert.Equal(t, dsn, "user:password@tcp(localhost:4000)/database?charset=utf8mb4&parseTime=True")
+	})
+
+	t.Run("test on build clickhouse dsn", func(t *testing.T) {
+		connConfig := ConnectionConfig{
+			Host:     "localhost",
+			Port:     "4000",
+			Username: "user",
+			Password: "password",
+			Database: "database",
+		}
+
+		dsn, err := connConfig.DSN(ClickHouse)
+		assert.NoError(t, err)
+
+		assert.Equal(t, dsn, "tcp://user:password@localhost:4000?database=database")
+	})
+
 }
